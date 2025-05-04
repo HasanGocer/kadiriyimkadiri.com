78a8cbcc3cad8655b33d49d2193aa5753cd5e6a1
Dennis Creevey <95920143+HasanGocer@users.noreply.github.com>
2025-05-05 02:21:25 +0300

    HG

--- Backend/db.js
+++ Backend/db.js
@@ -1,15 +1,14 @@
 import mysql from "mysql2/promise"; // mysql2/promise modülünü dahil ettik
-import dotenv from "dotenv";
 
 dotenv.config();
 
 // MySQL veritabanı bağlantısı
 const db = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "evpuser",
-  password: process.env.DB_PASSWORD || "YeniSifren123!",
-  database: process.env.DB_NAME || "kadiriyim_db",
-  port: process.env.DB_PORT || 3306,
+  host: "localhost",
+  user: "evpuser",
+  password: "YeniSifren123!",
+  database: "kadiriyim_db",
+  port: 3306,
 });
 
 db.getConnection()
